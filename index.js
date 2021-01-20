da5a4740d0037af807034d7e05cc7596c73fc909
vamendez00 <veromendez@gmail.com>
2021-01-20 12:49:47 -0600

    Initial Submission

--- index.js
+++ index.js
@@ -160,10 +160,16 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-    /*your code here*/
+function filterByWord(arr, string){
+    const filteredArray = [];
+for (let i=0; i<arr.length; i++){
+    if (arr[i].includes(string)){
+        filteredArray.push(arr[i]);
+    }
 }
-
+return filteredArray;
+}
+console.log (filterbyWord(originalFlavors, 'Chocolate'));
 
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
 
